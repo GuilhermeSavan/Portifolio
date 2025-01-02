06c0f5a942dd039823249e7ffa13a6087eeb86cc
GuilhermeSavan <guilherme.savan@gmail.com>
2025-01-02 08:13:03 -0300

    add initial About section

---
+++ react-portfolio/src/components/about/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './about.css'
+
+const About = () => {
+  return (
+      <main className="container">
+        <h3>Sobre min</h3>
+        <div className="about_text">
+          <p>Olá, sou Guilherme Savan, formado em Engenharia 
+            de Computaçãoe atualmente cursando Engenharia de 
+            Software.
+          </p>
+          <p>Tenho conhecimento em desenvolvimento web, explorando
+            tecnologias como React, JavaScript e Java. Este portfólio
+            apresenta minhas habilidades e meu progresso contínuo
+            no aprendizado dessas ferramentas.     
+          </p>
+          <p>Sou uma pessoa curiosa e determinada, sempre em 
+            busca de novos desafios que me permitam crescer e 
+            ampliar meu repertório técnico.
+          </p>
+        </div>
+      </main>
+  )
+}
+
+export default About
